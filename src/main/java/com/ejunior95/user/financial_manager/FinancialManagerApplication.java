0c30d6045ef267731a0c5894e595b7cc774f0b91
Edvaldo de Ramos Junior <edvaldo.ramos@castgroup.com.br>
2025-11-16 11:46:04 -0300

    feat: commit inicial

---
+++ src/main/java/com/ejunior95/user/financial_manager/FinancialManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ejunior95.user.financial_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejunior95/user/financial_manager/FinancialManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejunior95.user.financial_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
